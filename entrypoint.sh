ae00edc539e988cd0014090741b4540928924375
metrakit <contact@jordane.net>
2019-12-28 19:21:56 +0100

    fix cpu

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+export CPU_MHZ=2000
+
 if [ ! -f "./start_linux.sh" ]; then
-    export CPU_MHZ=2000
     /srv/steamcmd.sh  +login anonymous +force_install_dir /srv/OnsetServer +app_update 1204170 validate +quit
 fi
 
